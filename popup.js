afa08f69fb5debde85d3b54b3b1777f60a36566c
Aaronlong31 <aaron198809@gmail.com>
2013-07-24 10:15:39 +0800

    add readme

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 (function(){
     var apiKey = "44bc1c89c13a0743511d56b1cdd03502";
-    var apiUrl = "http://api.flickr.com/services/rest/?format=json&api_key=" + apiKey + "&nojsoncallback=1&method="
+    var apiUrl = "http://api.flickr.com/services/rest/?format=json&api_key=" + apiKey + "&nojsoncallback=1&method=";
     var getPhotoSetUrl = apiUrl + "flickr.photosets.getPhotos&photoset_id=";
-    var getPhotoUrl = apiUrl + "flickr.photos.getSizes&photo_id="
+    var getPhotoUrl = apiUrl + "flickr.photos.getSizes&photo_id=";
     var photos = [];
     var totalCount = 0;
     var loadedCount = 0;
-    
+
     function getPhotoSet(id) {
-      $("#loading").show();
-      $("#load").hide();
-      $.ajax({
+        $("#loading").show();
+        $("#load").hide();
+        $.ajax({
             url: getPhotoSetUrl + id,
             dataType: "json",
             async: false,
@@ -18,27 +18,27 @@
                 var ps = data.photoset.photo;
                 totalCount = ps.length;
                 $.each(ps, function(i, p){
-                  getPhoto(p.id);
+                    getPhoto(p.id);
                 });
             }
         });
     }
 
     function getPhoto(photoId) {
-      $.ajax({
-        url: getPhotoUrl + photoId,
-        dataType : "json",
-        success: function(pd) {
-          photos.push(pd.sizes.size);
-          if (++loadedCount == totalCount){
-            $("#loading").hide();
-            $("#load").show();
-          }
-          if (!sizeRadioShow) {
-            showSizeRadios(pd.sizes.size);
-          }
-        }
-      });
+        $.ajax({
+            url: getPhotoUrl + photoId,
+            dataType : "json",
+            success: function(pd) {
+                photos.push(pd.sizes.size);
+                if (++loadedCount == totalCount){
+                    $("#loading").hide();
+                    $("#load").show();
+                }
+                if (!sizeRadioShow) {
+                    showSizeRadios(pd.sizes.size);
+                }
+            }
+        });
     }
 
 
